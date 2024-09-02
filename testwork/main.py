6708bec5f087a6b26e97e484b31f40a7a44225f0
balyas <balyasnikovmisha@gmail.com>
2024-09-02 21:10:43 +0300

    01

--- testwork/main.py
+++ testwork/main.py
@@ -14,7 +14,5 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-print("xui")
-# uvicorn testwork.main:app
 
 # uvicorn testwork.main:app
